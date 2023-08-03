3e344a2c62a47987a14a490e9b5cae7427c6435f
Alex Dinh <dvthanh19@gmail.com>
2023-08-03 16:06:36 +0700

    16:06 Upload _ dvt

--- test_client.py
+++ test_client.py
@@ -1,7 +1,7 @@
 import socket
 import threading
-
-
+import pickle
+import json
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
@@ -10,32 +10,92 @@ nickname = input("Choose your nickname: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
 
+PM = False
+beginChatting = True
+nicknames = []
+beginChatting = True
+
+
 # Listening to Server and Sending Nickname
 def receive():
+    newnick = ""
     while True:
         try:
             # Receive Message From Server
-            # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+            dataDict = {
+                "text" : None,
+                "array": None
+            }
+            
+            # If 'getnickname' Send Nickname
+            data = client.recv(4096)
+            dataDict = json.loads(data.decode())
+
+            if dataDict["text"] == '\\get_nickname':
+                dataDict["text"] = nickname
+                newnick = nickname
+                client.sendall(json.dumps(dataDict).encode())
+
+            elif dataDict["text"] == '\\available_nickname':
+                print("**********")
+                print(dataDict["array"])
+                while (newnick in dataDict["array"]):
+                    # print("\'{}\' is available".format(newnick))
+                    print("Again")
+                    newnick = input("Try a new nickname: ")
+                dataDict["text"] = newnick
+                client.sendall(json.dumps(dataDict).encode())
+
+            elif dataDict["text"] == '\\update_list':
+                global nicknames
+                nicknames = dataDict["array"]
+                print(nicknames)
+                
+            elif dataDict["text"] == '\\close_all':
+                client.close()
+                break
             else:
-                print(message)
+                print(dataDict["text"],)
         except:
             # Close Connection When Error
             print("An error occured!")
             client.close()
             break
 
+
+
+
 # Sending Messages To Server
 def write():
+    dataDict = {
+        "text" : None,
+        "array": None
+    }
+
     while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        takenInput = input('')
+
+        if (takenInput[:8] == "\\online"):
+            print(nicknames)
+            continue
+        
+        if (takenInput[:4] == '\\pm '):
+            dataDict["text"] = takenInput
+        else:
+            dataDict["text"] = '{}: {}'.format(nickname, takenInput)
+
+        client.sendall(json.dumps(dataDict).encode())
+
+
+
+
+
+
+
 
 # Starting Threads For Listening And Writing
-receive_thread = threading.Thread(target=receive)
+receive_thread = threading.Thread(target = receive)
 receive_thread.start()
 
-write_thread = threading.Thread(target=write)
+write_thread = threading.Thread(target = write)
 write_thread.start()
--- test_server.py
+++ test_server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
-
+import pickle
+import json
 
 # Connection Data
 host = '127.0.0.1'
@@ -14,52 +15,132 @@ server.listen()
 # Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
-
+nickname = ''
 
 # Sending Messages To All Connected Clients
-def broadcast(message):
+def broadcast(message, sender):
     for client in clients:
-        client.send(message)
+        if client != sender:
+            client.sendall(message)
+
+def broadcastAll(message):
+    for client in clients:
+            client.sendall(message)
+
+def broadcastCList():
+    dataDict = {}
+    dataDict["text"] = '\\update_list'
+    dataDict["array"] = nicknames
+    for client in clients:
+            client.sendall(json.dumps(dataDict).encode())
+
+
+def privateChat():
+    print("privateChat()")
 
 
 # Handling Messages From Clients
 def handle(client):
     while True:
+        dataDict = {
+            "text" : None,
+            "array": None
+        }
         try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
+            
+            data = client.recv(4096)
+            dataDict = json.loads(data.decode())
+            idx = clients.index(client)
+            message = dataDict["text"]
+
+            # Private chat handle
+            if (message[:4] == "\\pm "):
+                rcvNick = message[message.find('<') + 1 : message.find('>')]
+                if not (rcvNick in nicknames):
+                    dataDict["text"] = ">> \'{}\' is not existed!".format(rcvNick)
+                    client.sendall(json.dumps(dataDict).encode())
+                else:
+                    dataDict["text"] = "{} (PM): ".format(nicknames[idx]) + message[message.find('-m ') + 3:]
+                    idx = nicknames.index(rcvNick)
+                    rcver = clients[idx]
+                    rcver.sendall(json.dumps(dataDict).encode())
+            else:
+                # Broadcasting Messages
+                broadcast(json.dumps(dataDict).encode(), client)
+                
+                # Out room handle
+                if (dataDict["text"] == nicknames[idx] + ': bye!'):
+                    clients.remove(client)
+                    dataDict["text"] = '\\close_all'
+                    client.sendall(json.dumps(dataDict).encode())
+                    client.close()
+                    nickname = nicknames[idx]
+
+                    dataDict["text"] = '>> {} left!'.format(nickname)
+                    broadcast(json.dumps(dataDict).encode(), client)
+                    
+                    nicknames.remove(nickname)
+                    break
         except:
             # Removing And Closing Clients
-            index = clients.index(client)
+            idx = clients.index(client)
             clients.remove(client)
             client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
+            nickname = nicknames[idx]
+            dataDict["text"] = '{} left!'.format(nickname)
+            broadcast(json.dumps(dataDict).encode(), client)
+            
+            nicknames.remove(nicknames[idx])
             break
 
 
 # Receiving / Listening Function
 def receive():
+    dataDict = {
+        "text" : None,
+        "array": None
+    }
+
+
     while True:
         # Accept Connection
         client, address = server.accept()
         print("Connected with {}".format(str(address)))
-
+        
         # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        dataDict["text"] = '\\get_nickname'
+        client.sendall(json.dumps(dataDict).encode())
+
+
+        # Receive nick name from client
+        data = client.recv(4096)
+        dataDict = json.loads(data.decode())
+        nickname = dataDict["text"]
+        if nickname in nicknames:
+            dataDict["text"] = "\\available_nickname"
+            dataDict["array"] = nicknames
+            client.sendall(json.dumps(dataDict).encode())
+            data = client.recv(4096)
+            dataDict = json.loads(data.decode())
+
         nicknames.append(nickname)
         clients.append(client)
 
         # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+
+        dataDict["text"] = ">> {} joined!".format(nickname)
+        broadcast(json.dumps(dataDict).encode(), client)
+
+        broadcastCList()
+        
+        # broadcastList()
+
+        dataDict["text"] = '>> Connected to server!'
+        client.sendall(json.dumps(dataDict).encode())
 
         # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target = handle, args = (client,))
         thread.start()
 
 
