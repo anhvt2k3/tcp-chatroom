0c131ca44edc44a7ab47aed710da0ba86f9cd40b
Alex Dinh <dvthanh19@gmail.com>
2023-08-09 16:15:17 +0700

    Add sending file function

--- test_client.py
+++ test_client.py
@@ -1,7 +1,9 @@
 import socket
 import threading
-import pickle
 import json
+import tqdm
+import os
+import math
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
@@ -10,50 +12,177 @@ nickname = input("Choose your nickname: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
 
-PM = False
-beginChatting = True
-nicknames = []
-beginChatting = True
-startToWrite = False
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 1024
 
+inMainRoom = True
+savedMessage = [[]]
+nicknames = []
+startToWrite = False
+file_path = ""
+file_size = - 1
+times = -1
+
+# Get the file_name in the file_path
+def fPath2fName(file_path):
+    if ('/' in file_path):
+        tmp = file_path[::-1]
+        tmp = tmp[: tmp.find('/')]
+        return tmp[::-1]
+
+    elif ('\\' in file_path):
+        tmp = file_path[::-1]
+        tmp = tmp[: tmp.find('\\')]
+        return tmp[::-1]
+    else:
+        return file_path    
+
+
+# \sendF text.txt
+# \sendF <d> text.txt
+# \sendF img.png
+
+def sendF_func(file_name):
+    global startToWrite
+    global times
+    startToWrite = False
+    
+    print(">> Ready to send file {}".format(file_name))
+
+    times =  math.ceil(int(file_size)/BUFFER_SIZE)
+    # print("times = {}".format(times))
+    with open(file_path, 'rb') as f:
+        for i in range(times):
+            data = f.read(1024)
+            client.sendall(data)
+            # print("i = {}".format(i))
+
+    # client.send("<END>".encode())
+    startToWrite = True
+    print(">> Upload {} successfully !!!".format(file_name))
+
+def rcvF_func(dataDict):
+    global startToWrite
+    startToWrite = False
+
+    message = dataDict['text']
+    sendNick = message[message.find(' <') + 2 : message.find('> ')]
+    file_name = message[message.find('-f ') + 3 : message.find('-rcv') - 1]
+    folder = "folder_" + nickname.replace(" ", "")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    file_path = folder + '/' + file_name
+    file_size = int(message[message.find(' (') + 2 : message.find(') ') ])
+
+
+    startToWrite = False
+    with open(file_path, 'wb') as f:
+        times = math.ceil(int(file_size)/BUFFER_SIZE)
+        # print("times = {}".format(times))
+        for i in range(times):
+            data = client.recv(1024)
+            f.write(data)
+            # print("i = {}".format(i))
+
+
+    startToWrite = True
+    # print(">> File received and saved as:", file_path)
+
+    # Print out 
+    if ("public" in message):
+        print ("{}: sent file {},\n saved as {}".format(sendNick, file_name, file_path))
+    else:
+        print ("{} (PM): sent file {},\n saved as".format(sendNick, file_name, file_path))
+
+
+
+def getNickname(dataDict):
+    global nicknames
+    global nickname
+    global startToWrite
+
+    nicknames = dataDict['array']
+    print("Online users list: {}".format(nicknames))
+    while (nickname in nicknames):
+        print("Nickname \'{}\' is used".format(nickname))
+        nickname = input(">> Try a new nickname: ")
+
+    dataDict['text'] = nickname
+    startToWrite = True
+    client.sendall(json.dumps(dataDict).encode())
+    
 
 # Listening to Server and Sending Nickname
 def receive():
     global nicknames
     global nickname
     global startToWrite
+
+    dataDict = {
+        'text' : None,
+        'array': None
+    }
+
     while True:
         try:
             # Receive Message From Server
-            dataDict = {
-                "text" : None,
-                "array": None
-            }
             
             # If 'getnickname' Send Nickname
             data = client.recv(4096)
             dataDict = json.loads(data.decode())
 
-            if dataDict["text"] == '\\get_nickname':
-                nicknames = dataDict["array"]
+            if dataDict['text'] == '\\get_nickname':
+                getNickname(dataDict)
+                """ 
+                nicknames = dataDict['array']
                 print("Online users list: {}".format(nicknames))
                 while (nickname in nicknames):
                     print("Nickname \'{}\' is used".format(nickname))
                     nickname = input(">> Try a new nickname: ")
 
-                dataDict["text"] = nickname
+                dataDict['text'] = nickname
                 startToWrite = True
                 client.sendall(json.dumps(dataDict).encode())
+                """
             
+            elif dataDict['text'][:7] == "\\sendF ":
+                rcvF_func(dataDict)
+                # message = dataDict['text']
+                # sendNick = message[message.find(' <') + 2 : message.find('> ')]
+                # file_name = message[message.find('-f ') + 3 : message.find('-rcv') - 1]
+                # folder = "folder_" + nickname.replace(" ", "")
+                # if not os.path.exists(folder):
+                #     os.makedirs(folder)
+                # file_path = folder + '/' + file_name
+                # # print(file_path)
+                # file_size = int(message[message.find(' (') + 2 : message.find(') ') ])
+                # startToWrite = False
+                # with open(file_path, 'wb') as f:
+                #     times = math.ceil(int(file_size)/BUFFER_SIZE)
+                #     # print("times = {}".format(times))
+                #     for i in range(times):
+                #         data = client.recv(1024)
+                #         f.write(data)
+                #         # print("i = {}".format(i))
+                # startToWrite = True
+                # # print(">> File received and saved as:", file_path)
 
-            elif dataDict["text"] == '\\update_list':
-                nicknames = dataDict["array"]
+                # if ("public" in message):
+                #     print ("\'{}\' sent file {}, saved as {}".format(sendNick, file_name, file_path))
+                # else:
+                #     print ("\'{}\' (PM) sent file {}, saved as".format(sendNick, file_name, file_path))
+            
+            elif dataDict['text'][:7] == "\\ready":
+                sendF_func("")
+
+            elif dataDict['text'] == '\\update_list':
+                nicknames = dataDict['array']
                 
-            elif dataDict["text"] == '\\close_all':
+            elif dataDict['text'] == '\\close_all':
                 client.close()
                 break
             else:
-                print(dataDict["text"],)
+                print(dataDict['text'],)
         except:
             # Close Connection When Error
             print("An error occured!")
@@ -62,15 +191,20 @@ def receive():
 
 
 
-
 # Sending Messages To Server
 def write():
     dataDict = {
-        "text" : None,
-        "array": None
+        'text' : None,
+        'array': None
     }
 
+    global file_path
+    global file_size
+    global times
+
     while True:
+        while (not startToWrite):
+            pass
         takenInput = input('')
 
         if (takenInput[:8] == "\\online"):
@@ -78,10 +212,24 @@ def write():
             continue
 
         # To pm:  "\pm <recv's name>  message"
-        if (takenInput[:4] == '\\pm '):
-            dataDict["text"] = takenInput
+        elif (takenInput[:4] == "\\pm "):
+            dataDict['text'] = takenInput
+
+        # To send file "\sendF <@all> file's path"
+        elif (takenInput[:7] == "\\sendF "):
+            if (not ('<' in takenInput)):
+                takenInput = takenInput[:7] + "<@all> " + takenInput[7:]
+            
+            file_path = takenInput[takenInput.find("> ") + 2:]
+            # print("<{}>".format(file_path))
+            file_size = os.path.getsize(file_path)
+            
+            dataDict['text'] = "\\sendF <{}> ({}) -f {}".format(takenInput[takenInput.find('<') + 1: takenInput.find('>')], str(file_size), fPath2fName(file_path))
+            print(dataDict['text'])
+            client.sendall(json.dumps(dataDict).encode())
+            continue
         else:
-            dataDict["text"] = '{}: {}'.format(nickname, takenInput)
+            dataDict['text'] = '{}: {}'.format(nickname, takenInput)
 
         client.sendall(json.dumps(dataDict).encode())
 
@@ -90,9 +238,6 @@ def write():
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target = receive)
 receive_thread.start()
-
-while (not startToWrite):
-    pass
 
 write_thread = threading.Thread(target = write)
 write_thread.start()
--- test_server.py
+++ test_server.py
@@ -1,11 +1,15 @@
 import socket
 import threading
-import pickle
 import json
+import tqdm
+import os
+import math
 
 # Connection Data
 host = '127.0.0.1'
 port = 55555
+
+BUFFER_SIZE = 1024
 
 # Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,6 +20,7 @@ server.listen()
 clients = []
 nicknames = []
 nickname = ''
+times = -1
 
 # Sending Messages To All Connected Clients
 def broadcast(message, sender):
@@ -29,57 +34,145 @@ def broadcastAll(message):
 
 def broadcastCList():
     dataDict = {}
-    dataDict["text"] = '\\update_list'
-    dataDict["array"] = nicknames
+    dataDict['text'] = '\\update_list'
+    dataDict['array'] = nicknames
     for client in clients:
             client.sendall(json.dumps(dataDict).encode())
 
 
-def privateChat():
-    print("privateChat()")
+def sendF2C(file_path, file_name, sender, pm = False, rcvNick = ""):
+    dataDict = {
+        'text' : None,
+        'array': None
+    }
+
+    # print("\'{}\' _ \'{}\' _ {}".format(file_path, file_name, sender))
+    idx = clients.index(sender)
+    sendNick = nicknames[idx]
+
+    if (not pm):
+        file_size = os.path.getsize(file_path)
+        dataDict['text'] = "\sendF <{}> ({}) -f {} -rcv public".format(sendNick, file_size, file_name)
+        broadcast(json.dumps(dataDict).encode(), sender)
+
+        with open(file_path, 'rb') as f:
+            for i in range(times):
+                data = f.read(1024)
+                broadcast(data, sender)
+                # print("i = {}".format(i))
+
+    else:
+        idx = nicknames.index(rcvNick)
+        rcver = clients[idx]
+        
+        file_size = os.path.getsize(file_path)
+        dataDict['text'] = "\sendF <{}> ({}) -f {} -rcv".format(sendNick, file_size, file_name)
+        rcver.sendall(json.dumps(dataDict).encode())  
+                  
+        with open(file_path, 'rb') as f:              
+            for i in range(times):
+                data = f.read(1024)
+                rcver.sendall(data)
+
+    print("Done transfering file {}".format(file_name))
+
 
 
 # Handling Messages From Clients
 def handle(client):
-    while True:
-        dataDict = {
-            "text" : None,
-            "array": None
+    global times
+    dataDict = {
+            'text' : None,
+            'array': None
         }
+    while True:
+        
         try:
             
             data = client.recv(4096)
             dataDict = json.loads(data.decode())
             idx = clients.index(client)
-            message = dataDict["text"]
+            message = dataDict['text']
 
             # Private chat handle
             if (message[:4] == "\\pm "):
                 rcvNick = message[message.find('<') + 1 : message.find('>')]
                 if not (rcvNick in nicknames):
-                    dataDict["text"] = ">> \'{}\' is not existed!".format(rcvNick)
+                    dataDict['text'] = ">> \'{}\' is not existed!".format(rcvNick)
                     client.sendall(json.dumps(dataDict).encode())
                 elif (rcvNick == clients[idx]):
-                    dataDict["text"] = "Cannot send message to yourself"
+                    dataDict['text'] = "Cannot send message to yourself"
                     client.sendall(json.dumps(dataDict).encode())
                 else:
-                    dataDict["text"] = "{} (PM): ".format(nicknames[idx]) + message[message.find('> ') + 2:]
+                    dataDict['text'] = "{} (PM): ".format(nicknames[idx]) + message[message.find('> ') + 2:]
                     idx = nicknames.index(rcvNick)
                     rcver = clients[idx]
                     rcver.sendall(json.dumps(dataDict).encode())
+            
+
+            elif (message[:7] == "\\sendF "):
+                print("get sendF successfully")
+                file_name = message[message.find('-f ') + 3:]
+                # print(file_name)
+                folder = "folder_server"
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                file_path = folder + '/' + file_name
+
+                file_size = message[message.find(' (') + 2: message.find(') ')]
+                # print(file_size)
+
+                dataDict['text'] = '\\ready'
+                client.sendall(json.dumps(dataDict).encode())
+
+                print("Ready to receive ...")
+                times =  math.ceil(int(file_size)/BUFFER_SIZE)
+                # print("times = {}".format(times))
+                with open(file_path, 'wb') as f:
+                    """
+                    while True:
+                        data = client.recv(1024)
+                        if (not data or data.decode() == '<END>'):
+                            break
+                        f.write(data)
+                        print(i)
+                        i += 1
+                    """
+                    
+                    for i in range(times):
+                        data = client.recv(1024)
+                        f.write(data)
+                        print("i = {}".format(i))
+                
+                print(">> File received and saved as:", file_path)
+                print(">> Message: {}".format(message))
+
+                if message[:14] == "\\sendF <@all> ":
+                    sendF2C(file_path, file_name, client)
+                else:
+                    rcvNick = message[message.find('<') + 1 : message.find('>')]
+                    sendF2C(file_path, file_name, client, True, rcvNick)
+
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+                    print(">> File \'{}\' deleted.".format(file_name))
+                else:
+                    print(">> File \'{}\' not found.".format(file_name))
+
+
             else:
                 # Broadcasting Messages
                 broadcast(json.dumps(dataDict).encode(), client)
                 
                 # Out room handle
-                if (dataDict["text"] == nicknames[idx] + ': bye!'):
+                if (dataDict['text'] == nicknames[idx] + ': bye!'):
                     clients.remove(client)
-                    dataDict["text"] = '\\close_all'
+                    dataDict['text'] = '\\close_all'
                     client.sendall(json.dumps(dataDict).encode())
                     client.close()
                     nickname = nicknames[idx]
 
-                    dataDict["text"] = '>> {} left!'.format(nickname)
+                    dataDict['text'] = '>> {} left!'.format(nickname)
                     broadcast(json.dumps(dataDict).encode(), client)
                     
                     nicknames.remove(nickname)
@@ -90,18 +183,19 @@ def handle(client):
             clients.remove(client)
             client.close()
             nickname = nicknames[idx]
-            dataDict["text"] = '{} left!'.format(nickname)
+            dataDict['text'] = '{} left!'.format(nickname)
             broadcast(json.dumps(dataDict).encode(), client)
             
             nicknames.remove(nicknames[idx])
             break
 
 
+
 # Receiving / Listening Function
 def receive():
     dataDict = {
-        "text" : None,
-        "array": None
+        'text' : None,
+        'array': None
     }
 
 
@@ -111,15 +205,15 @@ def receive():
         print("Connected with {}".format(str(address)))
         
         # Request And Store Nickname
-        dataDict["text"] = '\\get_nickname'
-        dataDict["array"] = nicknames
+        dataDict['text'] = '\\get_nickname'
+        dataDict['array'] = nicknames
         client.sendall(json.dumps(dataDict).encode())
 
 
         # Receive nick name from client
         data = client.recv(4096)
         dataDict = json.loads(data.decode())
-        nickname = dataDict["text"]
+        nickname = dataDict['text']
 
         nicknames.append(nickname)
         clients.append(client)
@@ -127,12 +221,12 @@ def receive():
         # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
 
-        dataDict["text"] = ">> {} joined!".format(nickname)
+        dataDict['text'] = ">> {} joined!".format(nickname)
         broadcast(json.dumps(dataDict).encode(), client)
 
         broadcastCList()
 
-        dataDict["text"] = '>> Connected to server!'
+        dataDict['text'] = '>> Connected to server!'
         client.sendall(json.dumps(dataDict).encode())
 
         # Start Handling Thread For Client
